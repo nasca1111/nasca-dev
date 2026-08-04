7bb00b152d32c0e464f62f61b577fd8c11c3f208
nasca1111 <166389450+nasca1111@users.noreply.github.com>
2026-08-05 03:17:10 +0900

    fix flask code

--- app.py
+++ app.py
@@ -247,9 +247,8 @@ def delete_post(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        
-    socketio.run(
-            app,
+
+    app.run(
             host="0.0.0.0",
             port=5000,
             debug=False
